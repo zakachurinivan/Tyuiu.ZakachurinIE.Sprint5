a301b518ea09e80de56176ba0e98e17f50616d50
Иван Закачурин <vanekcarek43@gmail.com>
2025-11-24 14:42:23 +0500

    Task2

---
+++ Tyuiu.ZakachurinIE.Sprint5.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.ZakachurinIE.Sprint5.Task2.V12.Lib;
+using System.IO;
+
+
+namespace Tyuiu.ZakachurinIE.Sprint5.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = "C:\\Users\\Пользователь\\AppData\\Local\\Temp\\OutPutFileTask2.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool exit = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(true, exit);
+        }
+    }
+}
---
+++ Tyuiu.ZakachurinIE.Sprint5.Task2.V12/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.ZakachurinIE.Sprint5.Task2.V12.Lib;
+using System.IO;
+namespace Tyuiu.ZakachurinIE.Sprint5.Task2.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт # 5 | Выполнил: Закачурин И. Е. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Варинат #12                                                             *");
+            Console.WriteLine("* Выполнил: Закачурин Иван Евгеньевич | ИБКСб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* результат сохранить в текстовый файл                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] mtrx = new int[3, 3] { { -5, -5, 9 }, { -7, 3, -4 }, { 8, 7, 9 } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.SaveToFileTextData(mtrx));
+            Console.WriteLine("Готов");
+            Console.ReadKey();
+        }
+    }
+}
