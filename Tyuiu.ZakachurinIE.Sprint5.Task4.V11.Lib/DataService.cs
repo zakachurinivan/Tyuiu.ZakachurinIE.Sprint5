f94ed9e5a60d20e0ffe860a9ee81218ec49528c2
Иван Закачурин <vanekcarek43@gmail.com>
2025-11-25 18:41:11 +0500

    Task4.1

--- Tyuiu.ZakachurinIE.Sprint5.Task4.V11.Lib/DataService.cs
+++ Tyuiu.ZakachurinIE.Sprint5.Task4.V11.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.ZakachurinIE.Sprint5.Task4.V11.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
-            double x = double.Parse(strX, CultureInfo.InvariantCulture); ;
+            double x = double.Parse(strX, CultureInfo.InvariantCulture);
             double res = Math.Round(Math.Sin(x) + Math.Pow(x,2)/2, 3);
             return res;
         }
