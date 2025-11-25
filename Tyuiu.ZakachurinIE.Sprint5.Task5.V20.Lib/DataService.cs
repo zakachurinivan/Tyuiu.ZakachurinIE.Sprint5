f742221fa355f7b7406b607d471e436bad5f5e34
Иван Закачурин <vanekcarek43@gmail.com>
2025-11-25 22:48:00 +0500

    Task5

---
+++ Tyuiu.ZakachurinIE.Sprint5.Task5.V20.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ZakachurinIE.Sprint5.Task5.V20.Lib
+{
+    public class DataService : ISprint5Task5V20
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    int k = 0;
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if ((double.Parse(numbers[i], CultureInfo.InvariantCulture) > -10) && (double.Parse(numbers[i], CultureInfo.InvariantCulture) < 10))
+                        {
+                            res += double.Parse(numbers[i], CultureInfo.InvariantCulture);
+                            k += 1;
+                        }
+                        
+                    }
+                    res = res / k;
+                }
+                
+            }
+            return Math.Round(res,3);
+        }
+    }
+}
---
+++ Tyuiu.ZakachurinIE.Sprint5.Task5.V20/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.ZakachurinIE.Sprint5.Task5.V20.Lib;
+using System.IO;
+
+namespace Tyuiu.ZakachurinIE.Sprint5.Task5.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\Пользователь\DataSprint5\InPutDataFileTask5V20.txt";
+            Console.Title = "Спринт # 5 | Выполнил: Закачурин И. Е. | ИБКСб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Варинат #11                                                             *");
+            Console.WriteLine("* Выполнил: Закачурин Иван Евгеньевич | ИБКСб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* результат сохранить в текстовый файл                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.LoadFromDataFile(path));
+            Console.WriteLine("Completed");
+            Console.ReadKey();
+        }
+    }
+}
